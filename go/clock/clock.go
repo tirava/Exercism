8ba46513fe7f05defae20d926141d7ad3fb3e165
Eugene Klimov <tirava@gmail.com>
2019-07-26 19:25:45 +0300

    Clock ready

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,44 +1,51 @@
 // Package clock implements a clock that handles times without dates.
 package clock
 
-import "strconv"
+import (
+	"fmt"
+)
 
 // Clock type
-type Clock string
+type Clock struct {
+	hours   int
+	minutes int
+}
 
 // New is a "constructor".
 func New(hour, minute int) (c Clock) {
 	minutes := hour*60 + minute
+	if minutes < 0 {
+		return c.Subtract(-minutes)
+	}
 	return c.Add(minutes)
 }
 
 // Add minutes to Clock.
 func (c Clock) Add(minutes int) Clock {
-	hours := minutes / 60
-	if hours >= 24 {
-		hours %= 24
-	}
-	minutes = minutes - hours*60
-	if minutes >= 60 {
-		minutes %= 60
-	}
-	h := strconv.Itoa(hours)
-	m := strconv.Itoa(minutes)
-	if hours < 10 {
-		h = "0" + h
-	}
-	if minutes < 10 {
-		m = "0" + m
-	}
-	return Clock(h + ":" + m)
+	minutes += c.hours*60 + c.minutes
+	return c.normalize(minutes)
 }
 
-// Subtract minutes to Clock.
+// Subtract minutes from Clock.
 func (c Clock) Subtract(minutes int) Clock {
-
-	return ""
+	minutes = (c.hours*60+c.minutes-minutes)%1440 + 1440
+	return c.normalize(minutes)
 }
 
+// String is stringer for Clock type.
 func (c Clock) String() string {
-	return string(c)
+	return fmt.Sprintf("%02d:%02d", c.hours, c.minutes)
+}
+
+// normalize does some calculations for Add & Subtract.
+func (c Clock) normalize(minutes int) Clock {
+	c.hours = minutes / 60
+	if c.hours >= 24 {
+		c.hours %= 24
+	}
+	c.minutes = minutes - c.hours*60
+	if c.minutes >= 60 {
+		c.minutes %= 60
+	}
+	return c
 }
