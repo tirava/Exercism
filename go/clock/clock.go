8ca687dc46f862044975e47ad98861e7de385ef0
Eugene Klimov <tirava@gmail.com>
2019-08-08 19:39:16 +0300

    Clock new - int vmesto struct & new methods

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -5,35 +5,30 @@ import (
 	"fmt"
 )
 
-// Clock type
-type Clock struct {
-	hours   int
-	minutes int
-}
+type Clock int
+
+const (
+	minsInHour = 60
+	hoursInDay = 24
+	minsInDay  = minsInHour * hoursInDay
+)
 
 // New is a "constructor".
-func New(hour, minute int) (c Clock) {
-	return c.min2Clock((hour*60+minute)%1440 + 1440)
+func New(hour, minute int) Clock {
+	return Clock((hour*minsInHour+minute)%minsInDay+minsInDay) % minsInDay
 }
 
 // Add minutes to Clock.
 func (c Clock) Add(minutes int) Clock {
-	return c.min2Clock(minutes + c.hours*60 + c.minutes)
+	return c + Clock(minutes)
 }
 
 // Subtract minutes from Clock.
 func (c Clock) Subtract(minutes int) Clock {
-	return c.min2Clock((c.hours*60+c.minutes-minutes)%1440 + 1440)
+	return (c - Clock(minutes)%minsInDay + minsInDay) % minsInDay
 }
 
 // String is stringer for Clock type.
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hours, c.minutes)
-}
-
-// min2Clock converts minutes to Clock type.
-func (c Clock) min2Clock(minutes int) Clock {
-	c.hours = minutes / 60 % 24
-	c.minutes = minutes % 60
-	return c
+	return fmt.Sprintf("%02d:%02d", c/minsInHour%hoursInDay, c%minsInHour)
 }
