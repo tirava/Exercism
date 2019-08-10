b71fee19e0657644eb1742e1921da73e3e65675e
Eugene Klimov <tirava@gmail.com>
2019-08-10 11:40:39 +0300

    Parallell (Concurrent) letters Start

---
+++ go/parallel-letter-frequency/frequency.go
@@ -0,0 +1,20 @@
+// Package letter implements concurrency counting letters in some languages
+package letter
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
+
+func ConcurrentFrequency(s ...[]string) (m FreqMap) {
+
+	return
+}
