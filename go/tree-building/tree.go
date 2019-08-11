996c87ecfe06176e6ef0ff134dffc3203add3702
Eugene Klimov <tirava@gmail.com>
2019-08-11 22:37:11 +0300

    Tree exercise - Nabroski

--- go/tree-building/tree.go
+++ go/tree-building/tree.go
@@ -1,6 +1,8 @@
 // Package tree implements the tree building logic for an unsorted set of records.
 package tree
 
+import "fmt"
+
 type Node struct {
 	ID       int
 	Children []*Node
@@ -14,5 +16,28 @@ type Record struct {
 // Build builds tree.
 func Build(records []Record) (*Node, error) {
 
-	return nil, nil
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	node := &Node{}
+
+	println("-----------")
+
+	for _, record := range records {
+		// find all unique parents
+		// and append new Node fo every
+		// sort
+
+		if node.ID == record.Parent && record.ID != 0 {
+			node.Children = append(node.Children, &Node{ID: record.ID})
+		}
+
+		fmt.Println(record)
+		//if record.ID == 0 && record.Parent !=0 {
+		//	return nil, errors.New("error building tree")
+		//}
+	}
+
+	return node, nil
 }
