0e6f41f2679b59ca79c89b459fe747113baa5caa
Eugene Klimov <tirava@gmail.com>
2019-08-14 21:53:27 +0300

    Series - basic exercise

---
+++ go/series/series.go
@@ -0,0 +1,18 @@
+// Package series implements output all the contiguous substrings
+// of length n in that string in the order that they appear.
+package series
+
+// All returns a list of all substrings of s with length n.
+func All(n int, s string) (res []string) {
+	for i := 0; i <= len(s)-n; i++ {
+		res = append(res, UnsafeFirst(n, s[i:]))
+	}
+	return
+
+}
+
+// UnsafeFirst returns the first substring of s with length n.
+func UnsafeFirst(n int, s string) string {
+
+	return s[:n]
+}
