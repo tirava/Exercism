311f65f4c69217233d93ff059adf87541b72e68d
Eugene Klimov <tirava@gmail.com>
2019-08-16 10:51:05 +0300

    Phone numbers - work regexp

--- go/phone-number/phone-number.go
+++ go/phone-number/phone-number.go
@@ -18,7 +18,7 @@ func Number(n string) (string, error) {
 		}
 	}
 
-	re := regexp.MustCompile(`^1\d{10}$|^2\d{9}$`)
+	re := regexp.MustCompile(`^1[2-9]\d{2}[2-9]\d{6}$|^2\d{2}[2-9]\d{6}$`)
 
 	//println(nums)
 
