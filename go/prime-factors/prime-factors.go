3f18587b3c03acb98f2720cc1293c6d0c2e0ae2b
Eugene Klimov <tirava@gmail.com>
2019-08-17 13:30:11 +0300

    Prime-factors end

--- go/prime-factors/prime-factors.go
+++ go/prime-factors/prime-factors.go
@@ -1,24 +1,20 @@
 // Package prime implements computing the prime factors of a given natural number.
 package prime
 
-import "math"
-
 // Factors returns slice of factors.
-func Factors(n int64) (res []int64) {
+func Factors(n int64) []int64 {
 
-	res = make([]int64, 8)
+	res := []int64{}
 	var i int64
 
-	for i = 2; i < math.MaxInt64; i++ {
-		for {
-			if n%i == 0 {
-				n = n / i
-				res = append(res, i)
-			} else {
-				break
-			}
+	for i = 2; i <= n; {
+		if n%i == 0 {
+			n = n / i
+			res = append(res, i)
+			continue
 		}
+		i++
 	}
 
-	return
+	return res
 }
