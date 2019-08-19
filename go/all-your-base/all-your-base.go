17dbb91cb827be00ce0261d1cfd65ede047fbc5c
Eugene Klimov <tirava@gmail.com>
2019-08-19 23:10:43 +0300

    All-Your-Base begin

---
+++ go/all-your-base/all-your-base.go
@@ -0,0 +1,9 @@
+// Package allyourbase implements converting a number,
+// represented as a sequence of digits in one base, to any other base.
+package allyourbase
+
+// ConvertToBase returns 'a' converted to base 'b'.
+func ConvertToBase(inBase int, inDigits []int, outBase int) (out []int, err error) {
+
+	return
+}
