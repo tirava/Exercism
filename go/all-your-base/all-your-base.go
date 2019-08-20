70ac94fdb11c80cea04f6cc17c208216242453e7
Eugene Klimov <tirava@gmail.com>
2019-08-20 20:23:26 +0300

    All-Your-Base draft work

--- go/all-your-base/all-your-base.go
+++ go/all-your-base/all-your-base.go
@@ -2,41 +2,57 @@
 // represented as a sequence of digits in one base, to any other base.
 package allyourbase
 
+import (
+	"errors"
+)
+
 // ConvertToBase returns 'a' converted to base 'b'.
 func ConvertToBase(inBase int, inDigits []int, outBase int) (out []int, err error) {
+
+	if inBase < 2 {
+		return nil, errors.New("input base must be >= 2")
+	}
+
+	if outBase < 2 {
+		return nil, errors.New("output base must be >= 2")
+	}
 
 	l := len(inDigits) - 1
 	res := 0
 
 	for i, d := range inDigits {
-		//out = append(out, int(float64(d) * math.Pow(float64(inBase), float64(l - i))))
+		if d < 0 || d >= inBase {
+			return nil, errors.New("all digits must satisfy 0 <= d < input base")
+		}
 		res += d * intPow(inBase, l-i)
 	}
 
-	x1 := res
-	//y := 0
 	for i := 0; ; i++ {
-		x2 := x1 % outBase
-		//y += x2 * intPow(10, i) // -> res
-		out = append(out, x2)
-		x1 = x1 / outBase
-		if x1 < outBase {
-			//out = append(out, x2)
+		rem := res % outBase
+		out = append(out, rem)
+		res = res / outBase
+		if res == 0 {
 			break
 		}
 	}
 
-	//out = append(out, res)
+	l = len(out)
+	if l > 1 {
+		for i := 0; i < l/2; i++ {
+			out[i], out[l-i-1] = out[l-i-1], out[i]
+		}
+	}
 
 	return
 }
 
 func intPow(x, y int) int {
+	res := x
 	if y == 0 {
 		return 1
 	}
 	for ; y > 1; y-- {
-		x *= x
+		res *= x
 	}
-	return x
+	return res
 }
