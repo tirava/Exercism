5951a7bfb776ba635a88492aaa03e3ca55158825
Eugene Klimov <tirava@gmail.com>
2019-08-22 15:06:09 +0300

    Simple-cipher types & interfaces

--- go/simple-cipher/simple-cipher.go
+++ go/simple-cipher/simple-cipher.go
@@ -1,7 +1,31 @@
-// .
+// Package cipher implements a simple shift cipher like Caesar and a more secure substitution cipher.
 package cipher
 
-// .
-func () {
-	
+// Caesar is the base struct.
+type Caesar struct {
+}
+
+// NewCaesar returns exemplar of base struct.
+func NewCaesar() *Caesar {
+	return &Caesar{}
+}
+
+// Encode returns encoded string.
+func (c *Caesar) Encode(in string) string {
+	return ""
+}
+
+// Decode returns decoded string.
+func (c *Caesar) Decode(in string) string {
+	return ""
+}
+
+// NewShift returns shifted distance times string.
+func NewShift(distance int) *Caesar {
+	return &Caesar{}
+}
+
+// NewVigenere returns shifted key times shifted strings.
+func NewVigenere(key string) *Caesar {
+	return &Caesar{}
 }
