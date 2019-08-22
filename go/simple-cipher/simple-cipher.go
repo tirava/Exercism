8215a92c06c94b7b3e54631b2854b77552fd8d63
Eugene Klimov <tirava@gmail.com>
2019-08-22 18:15:56 +0300

    Simple-cipher work draft

--- go/simple-cipher/simple-cipher.go
+++ go/simple-cipher/simple-cipher.go
@@ -23,24 +23,27 @@ func (c *Caesar) Encode(in string) string {
 	outSB := strings.Builder{}
 	outSB.Grow(len(in))
 
-	in = strings.ToLower(in)
-	//s = unicode.ToLower(s)
-
-	if c.key == "" {
-		for _, s := range in {
-			if !unicode.IsLetter(s) {
-				continue
-			}
-			// to lower
-			s1 := s + c.shift
-			if s1 > 'z' {
-				s1 -= 26
-			} else if s1 < 'a' {
-				s1 += 26
-			}
-			outSB.WriteRune(s1)
+	var s1 rune
+	var j int
+	for _, s := range in {
+		if !unicode.IsLetter(s) {
+			continue
 		}
+		s = unicode.ToLower(s)
+		if c.key == "" {
+			s1 = s + c.shift
+		} else {
+			s1 = s + (rune(c.key[j%len(c.key)])-'a')*c.shift
+		}
+		if s1 > 'z' {
+			s1 -= 26
+		} else if s1 < 'a' {
+			s1 += 26
+		}
+		outSB.WriteRune(s1)
+		j++
 	}
+
 	return outSB.String()
 }
 
@@ -49,6 +52,9 @@ func (c *Caesar) Decode(in string) string {
 	c1 := NewCaesar()
 	if c.key == "" {
 		c1.shift = -c.shift
+	} else {
+		c1.key = c.key
+		c1.shift = -1
 	}
 	return c1.Encode(in)
 }
@@ -65,7 +71,23 @@ func NewShift(distance int) Cipher {
 
 // NewVigenere returns shifted key times shifted strings.
 func NewVigenere(key string) Cipher {
+	if len(key) == 0 {
+		return nil
+	}
+	a := 0
+	for _, s := range key {
+		if !unicode.IsLetter(s) || unicode.IsUpper(s) {
+			return nil
+		}
+		if s == 'a' {
+			a++
+		}
+	}
+	if a == len(key) {
+		return nil
+	}
 	c := NewCaesar()
 	c.key = key
+	c.shift = 1
 	return c
 }
