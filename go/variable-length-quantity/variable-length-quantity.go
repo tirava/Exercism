79d57d899d67a45c5146cdb2177a03d498e802bf
Eugene Klimov <tirava@gmail.com>
2019-08-24 16:02:34 +0300

    variable-length-quantity Decode

--- go/variable-length-quantity/variable-length-quantity.go
+++ go/variable-length-quantity/variable-length-quantity.go
@@ -1,14 +1,35 @@
 // Package variablelengthquantity implements variable length quantity encoding and decoding.
 package variablelengthquantity
 
+import "errors"
+
 // DecodeVarint returns decoded VLQ.
 func DecodeVarint(input []byte) (out []uint32, err error) {
 
-	//for _, b := range input {
-	//
-	//}
+	var outI uint64
 
-	return nil, nil
+	for i, in := range input {
+		flush := false
+		if i == len(input)-1 && in&0x80 == 0x80 {
+			err = errors.New("last byte incomplete")
+			return
+		}
+		if in&0x80 == 0 {
+			flush = true
+		}
+		in &= 0x7f
+		in <<= 1
+		outI <<= 8
+		outI |= uint64(in)
+		outI >>= 1
+
+		if flush {
+			out = append(out, uint32(outI))
+			outI = 0
+		}
+	}
+
+	return
 }
 
 // EncodeVarint returns encoded VLQ.
@@ -16,10 +37,10 @@ func EncodeVarint(input []uint32) (out []byte) {
 
 	for _, in := range input {
 		outB := make([]byte, 0, 5)
-		for i := uint32(0); i < 5; i++ {
-			rol := uint64((uint64(in) << i) >> (i * 8))
-			rolB := byte(rol)
-			if rolB == 0 && i > 0 && rol == 0 {
+		for i := uint(0); i < 5; i++ {
+			ror := uint64((uint64(in) << i) >> (i * 8))
+			rolB := byte(ror)
+			if rolB == 0 && i > 0 && ror == 0 {
 				break
 			}
 			if i == 0 {
