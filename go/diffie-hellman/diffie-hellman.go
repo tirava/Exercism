b5722f53d461484c82e8eeaea113ccb4eb708483
Eugene Klimov <tirava@gmail.com>
2019-08-25 17:16:55 +0300

    Diffie-Hellman: End

--- go/diffie-hellman/diffie-hellman.go
+++ go/diffie-hellman/diffie-hellman.go
@@ -8,38 +8,33 @@ import (
 )
 
 var (
-	rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
-	_0  = big.NewInt(0)
-	_1  = big.NewInt(1)
+	rnd    = rand.New(rand.NewSource(time.Now().UnixNano()))
+	_0, _1 = big.NewInt(0), big.NewInt(1)
 )
 
 // PrivateKey returns picked private key.
 func PrivateKey(p *big.Int) *big.Int {
-	ab := _0
 	for {
-		ab.Rand(rnd, p)
-		if ab.Cmp(_1) > 0 {
+		_0.Rand(rnd, p)
+		if _0.Cmp(_1) > 0 {
 			break
 		}
 	}
-	return ab
+	return _0
 }
 
 // PublicKey returns public keys.
 func PublicKey(ab, p *big.Int, g int64) *big.Int {
-	AB := _0
-	return AB.Exp(big.NewInt(g), ab, p)
+	return _0.Exp(big.NewInt(g), ab, p)
 }
 
 // SecretKey returns secret key.
 func SecretKey(ab, BA, p *big.Int) *big.Int {
-	AB := _0
-	return AB.Exp(BA, ab, p)
+	return _0.Exp(BA, ab, p)
 }
 
 // NewPair returns new pairs.
-func NewPair(p *big.Int, g int64) (ab, AB *big.Int) {
-	ab = PrivateKey(p)
-	AB = PublicKey(ab, p, g)
-	return
+func NewPair(p *big.Int, g int64) (*big.Int, *big.Int) {
+	ab := PrivateKey(p)
+	return ab, PublicKey(ab, p, g)
 }
