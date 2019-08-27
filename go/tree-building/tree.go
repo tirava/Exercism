d526f300a1e5fbce15ab7c07fdade4fece5afd1e
Eugene Klimov <tirava@gmail.com>
2019-08-27 16:30:57 +0300

    Tree exercise - mentor govno

--- go/tree-building/tree.go
+++ go/tree-building/tree.go
@@ -2,7 +2,7 @@
 package tree
 
 import (
-	"errors"
+	"fmt"
 	"sort"
 )
 
@@ -35,23 +35,22 @@ func Build(records []Record) (*Node, error) {
 	for i, record := range records {
 
 		if record.ID >= rlen {
-			return nil, errors.New("non-continuous")
-		}
-		if i|record.Parent == 0 {
-			continue
+			return nil, fmt.Errorf("non-continuous (%d)", record.ID)
 		}
 		if i == 0 && record.Parent != 0 {
-			return nil, errors.New("root node has parent")
+			return nil, fmt.Errorf("root node should not have a parent (%d)", record.Parent)
 		}
-		if record.ID <= record.Parent {
-			return nil, errors.New("higher id parent of lower id")
+		if i != 0 && record.Parent >= i {
+			return nil, fmt.Errorf("parent id (%d) should be lower than its own id (%d)", record.Parent, i)
 		}
 		if i != record.ID {
-			return nil, errors.New("duplicate found")
+			return nil, fmt.Errorf("duplicate found (%d)", record.ID)
 		}
 
 		nodes[i] = Node{ID: record.ID}
-		nodes[record.Parent].Children = append(nodes[record.Parent].Children, &nodes[i])
+		if i != 0 {
+			nodes[record.Parent].Children = append(nodes[record.Parent].Children, &nodes[i])
+		}
 	}
 
 	return &nodes[0], nil
