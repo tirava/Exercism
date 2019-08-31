5513c83d7cd9f92c99c4372b00f57fcf99ed6261
Eugene Klimov <tirava@gmail.com>
2019-08-31 22:51:55 +0300

    Matching-brackets

--- go/matching-brackets/matching-brackets.go
+++ go/matching-brackets/matching-brackets.go
@@ -7,9 +7,7 @@ func Bracket(in string) bool {
 	var stack []rune
 
 	for _, s := range in {
-		//println(string(s))
 		if s == '(' || s == '[' || s == '{' {
-			//println("append stack:", string(s))
 			stack = append(stack, s)
 			continue
 		}
@@ -18,20 +16,12 @@ func Bracket(in string) bool {
 				return false
 			}
 			pop := stack[len(stack)-1]
-			//println("pop stack:", string(pop))
-			stack = stack[0:len(stack)-1]
-			if (pop == '(' && s == ')') || (pop == '[' && s == ']') || (pop == '{' && s == '}') {
-				//println(string(pop), string(s))
-				println(pop, s)
-				continue
+			stack = stack[0 : len(stack)-1]
+			if !(pop == '(' && s == ')') && !(pop == '[' && s == ']') && !(pop == '{' && s == '}') {
+				return false
 			}
 		}
 	}
 
-	//fmt.Println(stack)
-	if len(stack) > 0 {
-		return false
-	}
-
-	return true
+	return !(len(stack) > 0)
 }
