065f445fc98f0a57631258d6c1c3d6cd78ea7729
Eugene Klimov <tirava@gmail.com>
2019-09-02 22:20:00 +0300

    Robot-name End - mentor zaebal

--- go/robot-name/robot-name.go
+++ go/robot-name/robot-name.go
@@ -23,25 +23,17 @@ const maxNumNames = 26 * 26 * 10 * 10 * 10 // 676000
 // Name returns robot name.
 func (r *Robot) Name() (string, error) {
 
-	// comment for benchmark
+	if r.name != "" {
+		return r.name, nil
+	}
+
 	if len(robots) >= maxNumNames {
 		return "", fmt.Errorf("no names for new robots: %d names already", len(robots))
 	}
 
-	if robots[r.name] {
-		return r.name, nil
-	}
-
-	for ok := true; ok; {
-		r.name = r.newName()
-		_, ok = robots[r.name]
-
-		// uncomment for benchmark
-		//if len(robots) >= maxNumNames {
-		//	num := rand.Intn(1000000) // increase x10 and %0xd below if error "reissued" occurred
-		//	r.name += fmt.Sprintf("%06d", num)
-		//	break
-		//}
+	r.name = newName()
+	for robots[r.name] {
+		r.name = newName()
 	}
 
 	robots[r.name] = true
@@ -49,7 +41,7 @@ func (r *Robot) Name() (string, error) {
 	return r.name, nil
 }
 
-func (r *Robot) newName() string {
+func newName() string {
 	r1 := string(rand.Intn(26) + 'A')
 	r2 := string(rand.Intn(26) + 'A')
 	num := rand.Intn(1000)
@@ -58,7 +50,5 @@ func (r *Robot) newName() string {
 
 // Reset resets robot name.
 func (r *Robot) Reset() {
-	if r.name != "" {
-		robots[r.name] = false
-	}
+	r.name = ""
 }
