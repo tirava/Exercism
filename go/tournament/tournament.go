07db3fd2fcf048a2f80ff9d0823a44ca22446e00
Eugene Klimov <tirava@gmail.com>
2019-09-05 21:59:34 +0300

    Tournament - small fixes

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -11,8 +11,8 @@ import (
 )
 
 const (
-	command1 = iota
-	command2
+	player1 = iota
+	player2
 	played
 )
 
@@ -23,8 +23,8 @@ type score struct {
 type result map[string]*score
 
 type resultPair struct {
-	command string
-	points  int
+	player string
+	points int
 }
 
 type byPoints []resultPair
@@ -33,10 +33,10 @@ type byPoints []resultPair
 func Tally(r io.Reader, w io.Writer) (err error) {
 
 	buf := new(bytes.Buffer)
-	_, _ = buf.ReadFrom(r)
-	//if err != nil {
-	//	return
-	//}
+	_, err = buf.ReadFrom(r)
+	if err != nil {
+		return
+	}
 	s := buf.String()
 
 	results := make(result)
@@ -44,7 +44,7 @@ func Tally(r io.Reader, w io.Writer) (err error) {
 	formatBody := "%-31s|%3d |%3d |%3d |%3d |%3d\n"
 
 	lines := strings.Split(s, "\n")
-	_, _ = fmt.Fprintf(w, formatHeader, "Team", "MP ", "W ", "D ", "L ", "P")
+	fmt.Fprintf(w, formatHeader, "Team", "MP ", "W ", "D ", "L ", "P")
 
 	for _, line := range lines {
 		if len(line) <= 1 || line[0] == '#' {
@@ -55,23 +55,23 @@ func Tally(r io.Reader, w io.Writer) (err error) {
 			return errors.New("incorrect commands")
 		}
 
-		if _, ok := results[result[command1]]; !ok {
-			results[result[command1]] = &score{}
+		if _, ok := results[result[player1]]; !ok {
+			results[result[player1]] = &score{}
 		}
-		if _, ok := results[result[command2]]; !ok {
-			results[result[command2]] = &score{}
+		if _, ok := results[result[player2]]; !ok {
+			results[result[player2]] = &score{}
 		}
 
 		switch result[played] {
 		case "win":
-			results[result[command1]].win++
-			results[result[command2]].loss++
+			results[result[player1]].win++
+			results[result[player2]].loss++
 		case "draw":
-			results[result[command1]].draw++
-			results[result[command2]].draw++
+			results[result[player1]].draw++
+			results[result[player2]].draw++
 		case "loss":
-			results[result[command1]].loss++
-			results[result[command2]].win++
+			results[result[player1]].loss++
+			results[result[player2]].win++
 		default:
 			return errors.New("incorrect result")
 		}
@@ -83,13 +83,12 @@ func Tally(r io.Reader, w io.Writer) (err error) {
 		pl[i] = resultPair{k, v.win*3 + v.draw}
 		i++
 	}
-	//sort.Sort(pl)
 	sort.Sort(sort.Reverse(pl))
 
 	for _, v := range pl {
-		_, _ = fmt.Fprintf(w, formatBody, v.command,
-			results[v.command].win+results[v.command].draw+results[v.command].loss,
-			results[v.command].win, results[v.command].draw, results[v.command].loss, v.points)
+		fmt.Fprintf(w, formatBody, v.player,
+			results[v.player].win+results[v.player].draw+results[v.player].loss,
+			results[v.player].win, results[v.player].draw, results[v.player].loss, v.points)
 	}
 
 	return
@@ -99,10 +98,9 @@ func (p byPoints) Len() int { return len(p) }
 
 func (p byPoints) Less(i, j int) bool {
 	if p[i].points == p[j].points {
-		return p[i].command > p[j].command
-	} else {
-		return p[i].points < p[j].points
+		return p[i].player > p[j].player
 	}
+	return p[i].points < p[j].points
 }
 
 func (p byPoints) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
