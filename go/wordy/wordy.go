4ae797a8fcccbbf1cdaa4b7a190d5109905730e8
Eugene Klimov <tirava@gmail.com>
2019-09-09 23:40:19 +0300

    wordy - end

--- go/wordy/wordy.go
+++ go/wordy/wordy.go
@@ -8,43 +8,78 @@ import (
 )
 
 // Answer returns answer.
-func Answer(q string) (int, bool) {
+func Answer(in string) (int, bool) {
 
-	q = strings.ReplaceAll(q, "?", "")
-	q = strings.ReplaceAll(q, " by", "")
+	in = strings.ReplaceAll(in, "?", "")
+	in = strings.ReplaceAll(in, " by", "")
+	words := strings.Split(in, " ")
+	var d1, d2, result int
+	var err error
 
-	s := strings.Split(q, " ")
-	if len(s) < 3 {
+	if len(words) < 3 {
 		return 0, false
 	}
 
-	d1, err := strconv.Atoi(s[2]) // 1st digit
-	if err != nil {
-		println(d1)
-		return 0, false
-	}
-	if len(s) < 4 {
+	if len(words) == 3 {
+		d1, err = strconv.Atoi(words[2])
+		if err != nil {
+			return 0, false
+		}
 		return d1, true
 	}
 
-	d2, err := strconv.Atoi(s[4]) // 2nd digit
-	if err != nil {
+	for i, s := range words {
+
+		switch s {
+		case "plus", "minus", "multiplied", "divided":
+			if i+1 > len(words)-1 {
+				return 0, false
+			}
+			if i+2 == len(words)-1 {
+				return 0, false
+			}
+			d1, err = strconv.Atoi(words[i-1])
+			if err != nil {
+				return 0, false
+			}
+			d2, err = strconv.Atoi(words[i+1])
+			if err != nil {
+				return 0, false
+			}
+		}
+
+		switch s {
+		case "plus":
+			if result == 0 {
+				result = d1 + d2
+			} else {
+				result += d2
+			}
+		case "minus":
+			if result == 0 {
+				result = d1 - d2
+			} else {
+				result -= d2
+			}
+		case "multiplied":
+			if result == 0 {
+				result = d1 * d2
+			} else {
+				result *= d2
+			}
+		case "divided":
+			if result == 0 {
+				result = d1 / d2
+			} else {
+				result /= d2
+			}
+		}
+
+	}
+
+	if result == 0 {
 		return 0, false
 	}
 
-	sign := s[3]
-	switch sign {
-	case "plus":
-		return d1 + d2, true
-	case "minus":
-		return d1 - d2, true
-	case "multiplied":
-		return d1 * d2, true
-	case "divided":
-		return d1 / d2, true
-	}
-
-	return 0, false
-
-	//return 0, false
+	return result, true
 }
