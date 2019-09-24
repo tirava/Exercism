cfa0cb200158ed3facd6f3cf1039e3eae54a75c4
Eugene Klimov <tirava@gmail.com>
2019-09-24 11:04:07 +0300

    Bob switch

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -10,17 +10,18 @@ func Hey(remark string) string {
 	remark = strings.TrimSpace(remark)
 	isQuestion := strings.HasSuffix(remark, "?")
 	isUpper := strings.ToUpper(remark) == remark
-	isNoLetters := strings.ToUpper(remark) == remark && strings.ToLower(remark) == remark
+	isNoLetters := isUpper && strings.ToLower(remark) == remark
 
-	if isUpper && isQuestion && !isNoLetters {
+	switch {
+	case isUpper && isQuestion && !isNoLetters:
 		return "Calm down, I know what I'm doing!"
-	} else if isUpper && !isNoLetters {
+	case isUpper && !isNoLetters:
 		return "Whoa, chill out!"
-	} else if isQuestion {
+	case isQuestion:
 		return "Sure."
-	} else if remark == "" {
+	case remark == "":
 		return "Fine. Be that way!"
+	default:
+		return "Whatever."
 	}
-
-	return "Whatever."
 }
