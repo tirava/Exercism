baf11712bdcf7aacd0e8d8866f545c6f152e8bcf
Eugene Klimov <tirava@gmail.com>
2019-10-13 22:54:33 +0300

    Bank Account work but need optimize

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -2,20 +2,32 @@
 // opening/closing, withdrawals, and deposits of money.
 package account
 
+import (
+	"sync"
+)
+
 // Account is the base account type.
 type Account struct {
-	balance int64
 	active  bool
+	balance int64
+	mux     *sync.Mutex
 }
 
 // Open opens account.
 func Open(initialDeposit int64) *Account {
-
-	return &Account{initialDeposit, true}
+	if initialDeposit < 0 {
+		return nil
+	}
+	return &Account{true, initialDeposit, &sync.Mutex{}}
 }
 
 // Close close account.
 func (a *Account) Close() (payout int64, ok bool) {
+	a.mux.Lock()
+	defer a.mux.Unlock()
+	if !a.active {
+		return 0, false
+	}
 	a.active = false
 	return a.balance, true
 }
@@ -30,6 +42,15 @@ func (a *Account) Balance() (balance int64, ok bool) {
 
 // Deposit deposits of money.
 func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
-
-	return 0, false
+	a.mux.Lock()
+	defer a.mux.Unlock()
+	if !a.active {
+		return 0, false
+	}
+	a.balance += amount
+	if a.balance < 0 {
+		a.balance = 0
+		return a.balance, false
+	}
+	return a.balance, true
 }
