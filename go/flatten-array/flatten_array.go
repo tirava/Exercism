d883daa61982d9f0753aa739f0381a13cc22d796
Eugene Klimov <tirava@gmail.com>
2019-10-26 23:04:33 +0300

    Flatten-array end

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,47 +1,54 @@
 // Package flatten implements single flattened list with all values except nil/null.
 package flatten
 
-import (
-	"reflect"
-)
+import "reflect"
 
-var out = make([]interface{}, 0)
-var out1 = make([]interface{}, 0)
+var out []interface{}
 var nested int
 
 // Flatten gets nested list and return a single flattened list.
 func Flatten(in interface{}) []interface{} {
-
-	var s reflect.Value
-	//i := 0
-
-	s = reflect.ValueOf(in)
 
 	if in == nil {
 		nested--
 		return nil
 	}
 
+	s := reflect.ValueOf(in)
+
 	switch reflect.TypeOf(in).Kind() {
 	case reflect.Slice:
-		//s = reflect.ValueOf(in)
-
 		for i := 0; i < s.Len(); i++ {
 			nested++
 			Flatten(s.Index(i).Interface())
 		}
 	default:
-		//fmt.Println("value:", s.Interface())
-		if s.Interface() == nil {
-			return nil
-		}
 		out = append(out, s.Interface())
 	}
+
 	nested--
 	if nested < 0 {
-		out1 = out
-		out = make([]interface{}, 0)
 		nested = 0
+		outRet := out
+		out = make([]interface{}, 0)
+		return outRet
 	}
-	return out1
+
+	return nil
 }
+
+//func Flatten(input interface{}) []interface{} {
+//	flat := make([]interface{}, 0)
+//	if nested, ok := input.([]interface{}); ok {
+//		for _, elem := range nested {
+//			if inner, ok := elem.([]interface{}); ok {
+//				flat = append(flat, Flatten(inner)...)
+//			} else if elem != nil {
+//				flat = append(flat, elem)
+//			}
+//		}
+//	} else if input != nil {
+//		flat = append(flat, input)
+//	}
+//	return flat
+//}
