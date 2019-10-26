e8097b81c98371c4a676645b5f7f66201c36f011
Eugene Klimov <tirava@gmail.com>
2019-10-26 23:37:57 +0300

    Flatten-array best

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,54 +1,49 @@
 // Package flatten implements single flattened list with all values except nil/null.
 package flatten
 
-import "reflect"
-
-var out []interface{}
-var nested int
+//var out []interface{}
+//var nested int
+//
+//// Flatten gets nested list and return a single flattened list.
+//func Flatten(in interface{}) []interface{} {
+//
+//	if in == nil {
+//		nested--
+//		return nil
+//	}
+//
+//	s := reflect.ValueOf(in)
+//
+//	switch reflect.TypeOf(in).Kind() {
+//	case reflect.Slice:
+//		for i := 0; i < s.Len(); i++ {
+//			nested++
+//			Flatten(s.Index(i).Interface())
+//		}
+//	default:
+//		out = append(out, s.Interface())
+//	}
+//
+//	nested--
+//	if nested < 0 {
+//		nested = 0
+//		outRet := out
+//		out = make([]interface{}, 0)
+//		return outRet
+//	}
+//
+//	return nil
+//}
 
 // Flatten gets nested list and return a single flattened list.
-func Flatten(in interface{}) []interface{} {
-
-	if in == nil {
-		nested--
-		return nil
-	}
-
-	s := reflect.ValueOf(in)
-
-	switch reflect.TypeOf(in).Kind() {
-	case reflect.Slice:
-		for i := 0; i < s.Len(); i++ {
-			nested++
-			Flatten(s.Index(i).Interface())
+func Flatten(input interface{}) []interface{} {
+	flat := make([]interface{}, 0)
+	for _, elem := range input.([]interface{}) {
+		if inner, ok := elem.([]interface{}); ok {
+			flat = append(flat, Flatten(inner)...)
+		} else if elem != nil {
+			flat = append(flat, elem)
 		}
-	default:
-		out = append(out, s.Interface())
 	}
-
-	nested--
-	if nested < 0 {
-		nested = 0
-		outRet := out
-		out = make([]interface{}, 0)
-		return outRet
-	}
-
-	return nil
+	return flat
 }
-
-//func Flatten(input interface{}) []interface{} {
-//	flat := make([]interface{}, 0)
-//	if nested, ok := input.([]interface{}); ok {
-//		for _, elem := range nested {
-//			if inner, ok := elem.([]interface{}); ok {
-//				flat = append(flat, Flatten(inner)...)
-//			} else if elem != nil {
-//				flat = append(flat, elem)
-//			}
-//		}
-//	} else if input != nil {
-//		flat = append(flat, input)
-//	}
-//	return flat
-//}
