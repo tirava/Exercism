5bf11fdfb2ee1b044a0000e32b7f0e35d96cd460
Eugene Klimov <tirava@gmail.com>
2019-12-31 15:29:21 +0300

    Beer song start

---
+++ go/beer-song/beer_song.go
@@ -0,0 +1,21 @@
+// Package beer implements Recite the lyrics to that beloved classic,
+// that field-trip favorite: 99 Bottles of Beer on the Wall.
+package beer
+
+// Verse returns one verse.
+func Verse(num int) (string, error) {
+
+	return "", nil
+}
+
+// Verses returns verses from upper to lower.
+func Verses(upper, lower int) (string, error) {
+
+	return "", nil
+}
+
+// Song returns all beer song.
+func Song() string {
+
+	return ""
+}
