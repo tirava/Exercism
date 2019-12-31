27261cec77a75cbab9238b4ebdeeb07fe021fa2d
Eugene Klimov <tirava@gmail.com>
2019-12-31 18:51:53 +0300

    Beer song best solution

--- go/beer-song/beer_song.go
+++ go/beer-song/beer_song.go
@@ -4,17 +4,8 @@ package beer
 
 import (
 	"errors"
-	"strconv"
+	"fmt"
 	"strings"
-)
-
-// n1 = 99-1 or 'No more', $n2 =  99-1 or 'no more', s = '' or 's'
-// n1-1 = 98-1, one = one or it
-const (
-	verse0991 = "$n1 bottle$s of beer on the wall, $n2 bottle$s of beer.\n"
-	verse01   = "Take $one down and pass it around,"
-	verse02   = "Go to the store and buy some more,"
-	verse0993 = " $n1-1 bottle$s of beer on the wall.\n"
 )
 
 // Verse returns one verse.
@@ -23,64 +14,37 @@ func Verse(numVerse int) (string, error) {
 		return "", errors.New("invalid verse number")
 	}
 
-	v1 := verse0991
-	v2 := verse01
-	if numVerse == 0 {
-		v2 = verse02
+	switch numVerse {
+	case 0:
+		return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n", nil
+	case 1:
+		return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", nil
+	case 2:
+		return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n", nil
+	default:
+		return fmt.Sprintf("%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.\n", numVerse, numVerse, numVerse-1), nil
 	}
-	v3 := verse0993
-
-	switch {
-	case numVerse > 2:
-		v3 = strings.Replace(v3, "$s", "s", -1)
-		fallthrough
-	case numVerse > 1:
-		v1 = strings.Replace(v1, "$s", "s", -1)
-		v2 = strings.Replace(v2, "$one", "one", 1)
-		v3 = strings.Replace(v3, "$n1-1", strconv.Itoa(numVerse-1), 1)
-		v3 = strings.Replace(v3, "$s", "", -1)
-		fallthrough
-	case numVerse > 0:
-		v1 = strings.Replace(v1, "$n1", strconv.Itoa(numVerse), 1)
-		v1 = strings.Replace(v1, "$n2", strconv.Itoa(numVerse), 1)
-		fallthrough
-	case numVerse == 1:
-		v1 = strings.Replace(v1, "$s", "", -1)
-		v2 = strings.Replace(v2, "$one", "it", 1)
-		v3 = strings.Replace(v3, "$n1-1", "no more", 1)
-		v3 = strings.Replace(v3, "$s", "s", -1)
-	default: // == 0
-		v1 = strings.Replace(v1, "$n1", "No more", 1)
-		v1 = strings.Replace(v1, "$n2", "no more", 1)
-		v1 = strings.Replace(v1, "$s", "s", -1)
-		v3 = strings.Replace(v3, "$n1-1", "99", 1)
-		v3 = strings.Replace(v3, "$s", "s", -1)
-	}
-
-	return v1 + v2 + v3, nil
 }
 
 // Verses returns verses from upper to lower.
 func Verses(upper, lower int) (string, error) {
-	if upper < lower {
+	if upper > 100 || lower < 0 || lower > upper {
 		return "", errors.New("start < stop")
 	}
-	out := ""
+	sb := strings.Builder{}
 	for i := upper; i >= lower; i-- {
 		v, err := Verse(i)
 		if err != nil {
 			return "", errors.New("invalid verses numbers")
 		}
-		out += v + "\n"
+		sb.WriteString(v)
+		sb.WriteString("\n")
 	}
-	return out, nil
+	return sb.String(), nil
 }
 
 // Song returns all beer song.
 func Song() string {
-	song, err := Verses(99, 0)
-	if err != nil {
-		return "invalid call of verses"
-	}
+	song, _ := Verses(99, 0)
 	return song
 }
