1e84e7489ae5ad3d36b65e9056abc158ae32bcf6
Eugene Klimov <tirava@gmail.com>
2019-12-31 18:16:10 +0300

    Beer song work verse draft

--- go/beer-song/beer_song.go
+++ go/beer-song/beer_song.go
@@ -2,10 +2,62 @@
 // that field-trip favorite: 99 Bottles of Beer on the Wall.
 package beer
 
-// Verse returns one verse.
-func Verse(num int) (string, error) {
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
 
-	return "", nil
+// n1 = 99-1 or 'No more', $n2 =  99-1 or 'no more', s = '' or 's'
+// n1-1 = 98-1, one = one or it
+const (
+	verse0991 = "$n1 bottle$s of beer on the wall, $n2 bottle$s of beer.\n"
+	verse01   = "Take $one down and pass it around,"
+	verse02   = "Go to the store and buy some more,"
+	verse0993 = " $n1-1 bottle$s of beer on the wall.\n"
+)
+
+// Verse returns one verse.
+func Verse(numVerse int) (string, error) {
+	if numVerse < 0 || numVerse > 99 {
+		return "", errors.New("invalid verse number")
+	}
+
+	v1 := verse0991
+	v2 := verse01
+	if numVerse == 0 {
+		v2 = verse02
+	}
+	v3 := verse0993
+
+	switch {
+	case numVerse > 2:
+		v3 = strings.Replace(v3, "$s", "s", -1)
+		fallthrough
+	case numVerse > 1:
+		v1 = strings.Replace(v1, "$s", "s", -1)
+		v2 = strings.Replace(v2, "$one", "one", 1)
+		v3 = strings.Replace(v3, "$n1-1", strconv.Itoa(numVerse-1), 1)
+		v3 = strings.Replace(v3, "$s", "", -1)
+		fallthrough
+	case numVerse > 0:
+		v1 = strings.Replace(v1, "$n1", strconv.Itoa(numVerse), 1)
+		v1 = strings.Replace(v1, "$n2", strconv.Itoa(numVerse), 1)
+		fallthrough
+	case numVerse == 1:
+		v1 = strings.Replace(v1, "$s", "", -1)
+		v2 = strings.Replace(v2, "$one", "it", 1)
+		v3 = strings.Replace(v3, "$n1-1", "no more", 1)
+		v3 = strings.Replace(v3, "$s", "s", -1)
+	default: // == 0
+		v1 = strings.Replace(v1, "$n1", "No more", 1)
+		v1 = strings.Replace(v1, "$n2", "no more", 1)
+		v1 = strings.Replace(v1, "$s", "s", -1)
+		v3 = strings.Replace(v3, "$n1-1", "99", 1)
+		v3 = strings.Replace(v3, "$s", "s", -1)
+	}
+
+	return v1 + v2 + v3, nil
 }
 
 // Verses returns verses from upper to lower.
