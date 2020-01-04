11ca714d6ea0e970ed864c0e619952556d717d85
Eugene Klimov <tirava@gmail.com>
2020-01-04 22:09:14 +0300

    house start

---
+++ go/house/house.go
@@ -0,0 +1,15 @@
+// Package house implements reciting the nursery rhyme
+// 'This is the House that Jack Built'.
+package house
+
+// Verse returns concrete verse of the song.
+func Verse(n int) string {
+
+	return ""
+}
+
+// Song returns all song.
+func Song() string {
+
+	return ""
+}
