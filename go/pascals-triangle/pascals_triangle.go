5b9563c1d2946f2c98e2e95f40bdbac1de06a4a6
Eugene Klimov <tirava@gmail.com>
2020-01-05 23:03:25 +0300

    triangle ready

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -6,7 +6,14 @@ func Triangle(n int) [][]int {
 	result := make([][]int, n)
 
 	for i := 0; i < n; i++ {
-		result[i] = make([]int, i)
+		result[i] = make([]int, i+1)
+		for j := 0; j <= i; j++ {
+			if j == 0 || j == i {
+				result[i][j] = 1
+				continue
+			}
+			result[i][j] = result[i-1][j-1] + result[i-1][j]
+		}
 	}
 
 	return result
