2b3e97dd4a27d5fce47c6bd0b5e2fb973b01ce9d
Eugene Klimov <tirava@gmail.com>
2020-01-07 21:40:20 +0300

    rail-fence-cipher ready but not optimized

--- go/rail-fence-cipher/rail_fence_cipher.go
+++ go/rail-fence-cipher/rail_fence_cipher.go
@@ -7,8 +7,59 @@ import (
 
 // Decode returns decoded string.
 func Decode(in string, n int) string {
+	arr := make([][]string, n)
+	for i := range arr {
+		arr[i] = make([]string, len(in))
+	}
+	indexH := 0
+	indexV := 0
+	indexZ := 1
+	for range in {
+		arr[indexV][indexH] = "?"
+		indexH++
+		if indexH >= len(in) {
+			indexH = 0
+		}
+		indexV += indexZ
+		if indexV >= n || indexV < 0 {
+			indexV -= indexZ
+			indexZ = -indexZ
+			indexV += indexZ
+		}
+	}
 
-	return ""
+	j := 0
+	inr := []rune(in)
+	for _, ar := range arr {
+		for i, s := range ar {
+			if s == "?" {
+				ar[i] = string(inr[j])
+				j++
+			}
+		}
+	}
+
+	indexH = 0
+	indexV = 0
+	indexZ = 1
+	out := ""
+	for range in {
+		out += arr[indexV][indexH]
+		indexH++
+		if indexH >= len(in) {
+			indexH = 0
+		}
+		indexV += indexZ
+		if indexV >= n || indexV < 0 {
+			indexV -= indexZ
+			indexZ = -indexZ
+			indexV += indexZ
+		}
+	}
+
+	//fmt.Println(arr)
+
+	return out
 }
 
 // Encode returns encoded string.
