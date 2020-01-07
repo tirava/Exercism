d7fb7964e8ae186ff89f2de4b90f2a84fca72b3a
Eugene Klimov <tirava@gmail.com>
2020-01-07 19:15:09 +0300

    rail-fence-cipher encode

--- go/rail-fence-cipher/rail_fence_cipher.go
+++ go/rail-fence-cipher/rail_fence_cipher.go
@@ -1,14 +1,44 @@
 // Package railfence implements encoding and decoding for the rail fence cipher.
 package railfence
 
+import (
+	"strings"
+)
+
 // Decode returns decoded string.
-func Decode(s string, i int) string {
+func Decode(in string, n int) string {
 
 	return ""
 }
 
 // Encode returns encoded string.
-func Encode(s string, i int) string {
+func Encode(in string, n int) string {
+	arr := make([][]string, n)
+	for i := range arr {
+		arr[i] = make([]string, len(in))
+	}
+	indexH := 0
+	indexV := 0
+	indexZ := 1
+	for _, s := range in {
+		arr[indexV][indexH] = string(s)
+		indexH++
+		if indexH >= len(in) {
+			indexH = 0
+		}
+		indexV += indexZ
+		if indexV >= n || indexV < 0 {
+			indexV -= indexZ
+			indexZ = -indexZ
+			indexV += indexZ
+		}
+	}
 
-	return ""
+	out := ""
+	for _, ar := range arr {
+		s := strings.Join(ar, "")
+		out += s
+	}
+
+	return out
 }
