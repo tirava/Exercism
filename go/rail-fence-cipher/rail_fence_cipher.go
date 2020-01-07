d6e7cbab324ea0836f5320184a66d32db9bf4cbe
Eugene Klimov <tirava@gmail.com>
2020-01-07 17:25:58 +0300

    rail-fence-cipher start

---
+++ go/rail-fence-cipher/rail_fence_cipher.go
@@ -0,0 +1,14 @@
+// Package railfence implements encoding and decoding for the rail fence cipher.
+package railfence
+
+// Decode returns decoded string.
+func Decode(s string, i int) string {
+
+	return ""
+}
+
+// Encode returns encoded string.
+func Encode(s string, i int) string {
+
+	return ""
+}
