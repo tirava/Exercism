08a10561b84d207954daafb564279faaad0d7a7a
Eugene Klimov <tirava@gmail.com>
2020-01-09 17:29:36 +0300

    circular buffer ready

--- go/circular-buffer/circular_buffer.go
+++ go/circular-buffer/circular_buffer.go
@@ -1,13 +1,17 @@
 // Package circular implements circular buffer, cyclic buffer or ring buffer.
 package circular
 
-import "errors"
+import (
+	"errors"
+)
 
+// Buffer is the base buffer type.
 type Buffer struct {
-	pointer int
-	size    int
-	free    int
-	buffer  []byte
+	begin  int
+	end    int
+	size   int
+	free   int
+	buffer []byte
 }
 
 // NewBuffer return new buffer.
@@ -24,11 +28,12 @@ func (b *Buffer) ReadByte() (byte, error) {
 	if b.free == b.size {
 		return 0, errors.New("buffer is empty")
 	}
-	read := b.buffer[b.pointer]
+	read := b.buffer[b.begin]
+	b.buffer[b.begin] = 0
 	b.free++
-	b.pointer--
-	if b.pointer < 0 {
-		b.pointer = b.size - 1
+	b.begin++
+	if b.begin > b.size-1 {
+		b.begin = 0
 	}
 	return read, nil
 }
@@ -39,20 +44,36 @@ func (b *Buffer) WriteByte(c byte) error {
 		return errors.New("buffer is full")
 	}
 	b.free--
-	b.pointer++
-	if b.pointer > b.size-1 {
-		b.pointer = 0
+	b.buffer[b.end] = c
+	b.end++
+	if b.end > b.size-1 {
+		b.end = 0
 	}
-	b.buffer[b.pointer] = c
 	return nil
 }
 
 // Overwrite overwrites byte in buffer.
 func (b *Buffer) Overwrite(c byte) {
-
+	if b.free > 0 {
+		_ = b.WriteByte(c)
+		return
+	}
+	b.begin++
+	if b.begin > b.size-1 {
+		b.begin = 0
+	}
+	b.buffer[b.end] = c
+	b.end++
+	if b.end > b.size-1 {
+		b.end = 0
+	}
 }
 
 // Reset puts buffer in empty state.
 func (b *Buffer) Reset() {
-
+	b.begin = 0
+	b.end = 0
+	b.free = b.size
+	b.buffer = nil
+	b.buffer = make([]byte, b.size)
 }
