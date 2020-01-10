3a9cf4eee59dd5b0be19414c117db89524304c7e
Eugene Klimov <tirava@gmail.com>
2020-01-10 11:27:56 +0300

    diamond ready

--- go/diamond/diamond.go
+++ go/diamond/diamond.go
@@ -1,8 +1,40 @@
 // Package diamond implements the diamond kata.
 package diamond
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 // Gen returns diamond.
 func Gen(b byte) (string, error) {
+	if b < 'A' || b > 'Z' {
+		return "", errors.New("invalid letter")
+	}
 
-	return "", nil
+	dia := strings.Builder{}
+
+	for i := byte('A'); i < b; i++ {
+		dia.WriteString(writeLine(i, b))
+	}
+	for i := b; i >= 'A'; i-- {
+		dia.WriteString(writeLine(i, b))
+	}
+
+	return dia.String(), nil
+}
+
+func writeLine(b, letters byte) string {
+	s1 := strings.Repeat(" ", int(letters-b))
+	s2 := strings.Repeat(" ", int((b-'A')*2-1))
+	bs := ""
+
+	if b == 'A' {
+		bs = string(b)
+	} else {
+		bs = fmt.Sprintf("%s%s%s", string(b), s2, string(b))
+	}
+
+	return fmt.Sprintf("%s%s%s\n", s1, bs, s1)
 }
