a6b15ae32ada3058e0fdc179cb85e9b965c770e7
Eugene Klimov <tirava@gmail.com>
2020-02-08 12:31:13 +0300

    saddle start

---
+++ go/saddle-points/matrix.go
@@ -0,0 +1,69 @@
+// Package matrix implements matrix of numbers, return the rows and columns of that matrix.
+package matrix
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+// Matrix is the base type.
+type Matrix [][]int
+
+// New returns new matrix and checks them corrections.
+func New(in string) (*Matrix, error) {
+
+	inRow := strings.Split(in, "\n")
+	m := make(Matrix, len(inRow))
+
+	for i, c := range inRow {
+		m[i] = make([]int, 0)
+		inCol := strings.Split(c, " ")
+		for _, s := range inCol {
+			if s == "" {
+				continue
+			}
+			inInt, err := strconv.Atoi(s)
+			if err != nil {
+				return nil, errors.New("bad matrix convert")
+			}
+			m[i] = append(m[i], inInt)
+		}
+		if i > 0 && len(m[i]) != len(m[i-1]) {
+			return nil, errors.New("bad matrix length")
+		}
+	}
+
+	return &m, nil
+}
+
+// Set sets matrix values.
+func (m Matrix) Set(row, col, val int) bool {
+	if row < 0 || col < 0 || row > len(m)-1 || col > len(m[0])-1 {
+		return false
+	}
+	m[row][col] = val
+	return true
+}
+
+// Rows returns duplicate Matrix rows.
+func (m Matrix) Rows() [][]int {
+	duplicate := make([][]int, len(m))
+	for i := range m {
+		duplicate[i] = make([]int, len(m[i]))
+		copy(duplicate[i], m[i])
+	}
+	return duplicate
+}
+
+// Cols returns duplicate Matrix columns.
+func (m Matrix) Cols() [][]int {
+	duplicate := make([][]int, len(m[0]))
+	for j := range m[0] {
+		duplicate[j] = make([]int, len(m))
+		for i := range m {
+			duplicate[j][i] = m[i][j]
+		}
+	}
+	return duplicate
+}
---
+++ go/saddle-points/saddle_points.go
@@ -0,0 +1,8 @@
+package matrix
+
+// Pair is the base pair type.
+type Pair [2]int
+
+func (m Matrix) Saddle() []Pair {
+	return nil
+}
