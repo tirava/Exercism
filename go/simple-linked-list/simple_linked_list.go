f6ad926f3111253e7ee3ce083f2a2b5d42cc93cf
Eugene Klimov <tirava@gmail.com>
2020-02-18 21:20:48 +0300

    simple list works

--- go/simple-linked-list/simple_linked_list.go
+++ go/simple-linked-list/simple_linked_list.go
@@ -1,6 +1,10 @@
 // Package linkedlist implements simple linked list that uses Elements and a List.
 package linkedlist
 
+import (
+	"errors"
+)
+
 // Element base type.
 type Element struct {
 	data int
@@ -15,30 +19,88 @@ type List struct {
 
 // New returns new list from slice.
 func New(from []int) *List {
-	return &List{}
+	list := &List{}
+
+	for _, data := range from {
+		list.Push(data)
+	}
+
+	return list
+}
+
+func (l *List) back(backIndex int) *Element {
+	back := l.head
+
+	for i := 0; i < l.size-(backIndex+1); i++ {
+		back = back.next
+	}
+
+	return back
 }
 
 // Size returns size of the list.
 func (l *List) Size() int {
-	return 0
+	return l.size
 }
 
 // Push adds element to the list.
-func (l *List) Push(elem int) {
+func (l *List) Push(data int) {
+	elem := &Element{data: data}
 
+	if l.size > 0 {
+		l.back(0).next = elem
+	} else {
+		l.head = elem
+	}
+
+	l.size++
 }
 
 // Pop removes element from the list.
 func (l *List) Pop() (int, error) {
-	return 0, nil
+	if l.size <= 0 {
+		return 0, errors.New("empty list")
+	}
+
+	var data int
+	if l.size == 1 {
+		data = l.head.data
+		l.head = nil
+	} else {
+		data = l.back(0).data
+		l.back(1).next = nil
+	}
+
+	l.size--
+
+	return data, nil
 }
 
 // Array returns slice from the list.
 func (l *List) Array() []int {
-	return nil
+	arr := make([]int, l.size)
+	next := l.head
+
+	for i := 0; i < l.size; i++ {
+		arr[i] = next.data
+		next = next.next
+	}
+
+	return arr
 }
 
 // Reverse reverses the list.
 func (l *List) Reverse() *List {
-	return &List{}
+	list := &List{}
+	s := l.Array()
+
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+
+	for _, v := range s {
+		list.Push(v)
+	}
+
+	return list
 }
