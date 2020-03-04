d13ae627ef35d4064002cc34f9e448758e60abe4
Eugene Klimov <tirava@gmail.com>
2020-03-04 12:47:45 +0300

    minesweeper ready

--- go/minesweeper/minesweeper.go
+++ go/minesweeper/minesweeper.go
@@ -1,9 +1,15 @@
 // Package minesweeper implements a minesweeper board.
 package minesweeper
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Count counts mines around.
 func (b Board) Count() error {
 	lenX, lenY := len(b[0]), len(b)
+
 	if lenX < 3 || lenY < 3 {
 		return nil
 	}
@@ -12,12 +18,25 @@ func (b Board) Count() error {
 		for y := 1; y < lenY-1; y++ {
 			s := b[y][x]
 
+			if len(b[y]) != lenX {
+				return errors.New("bad board line")
+			}
+
 			if s == ' ' {
 				var count byte
 
 				for xx := x - 1; xx <= x+1; xx++ {
 					for yy := y - 1; yy <= y+1; yy++ {
 						ss := b[yy][xx]
+
+						if (xx == lenX || yy == lenY || xx == 0 || yy == 0) && (ss != '+' && ss != '-' && ss != '|') {
+							return errors.New("bad board line")
+						}
+
+						if ss != '*' && ss != '+' && ss != '-' && ss != '|' && ss != ' ' && (ss < '0' || ss > '9') {
+							fmt.Println("bad:", ss, string(ss), xx, yy)
+							return errors.New("bad board")
+						}
 
 						if ss == '*' {
 							count++
