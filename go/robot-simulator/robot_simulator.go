6f87fd2791a0e0fb81ee923ead425e8c2c9c39c6
Eugene Klimov <tirava@gmail.com>
2020-03-05 17:32:51 +0300

    robot step1 ready

--- go/robot-simulator/robot_simulator.go
+++ go/robot-simulator/robot_simulator.go
@@ -7,24 +7,51 @@ func (d Dir) String() string {
 }
 
 // global directions.
-var (
-	N Dir
-	S Dir
-	W Dir
-	E Dir
+const (
+	N Dir = iota
+	S
+	W
+	E
 )
 
 // Left stepping.
 func Left() {
-
+	switch Step1Robot.Dir {
+	case N:
+		Step1Robot.Dir = W
+	case W:
+		Step1Robot.Dir = S
+	case S:
+		Step1Robot.Dir = E
+	case E:
+		Step1Robot.Dir = N
+	}
 }
 
 // Right stepping.
 func Right() {
-
+	switch Step1Robot.Dir {
+	case N:
+		Step1Robot.Dir = E
+	case E:
+		Step1Robot.Dir = S
+	case S:
+		Step1Robot.Dir = W
+	case W:
+		Step1Robot.Dir = N
+	}
 }
 
 // Advance for stepping.
 func Advance() {
-
+	switch Step1Robot.Dir {
+	case N:
+		Step1Robot.Y++
+	case S:
+		Step1Robot.Y--
+	case E:
+		Step1Robot.X++
+	case W:
+		Step1Robot.X--
+	}
 }
