997b732f7dc89651bef74a1f2d063f5bfe4f855c
Eugene Klimov <tirava@gmail.com>
2020-03-05 18:47:07 +0300

    robot step2 draft

--- go/robot-simulator/robot_simulator.go
+++ go/robot-simulator/robot_simulator.go
@@ -3,7 +3,7 @@ package robot
 
 // String is stringer for direction.
 func (d Dir) String() string {
-	return "111"
+	return string(d)
 }
 
 // global directions.
@@ -54,4 +54,47 @@ func Advance() {
 	case W:
 		Step1Robot.X--
 	}
+}
+
+// Action base type.
+type Action string
+
+// StartRobot turns robot on.
+func StartRobot(cmd chan Command, act chan Action) {
+	for c := range cmd {
+		switch c {
+		case 'R':
+			act <- "Right"
+		case 'L':
+			act <- "Left"
+		case 'A':
+			act <- "Advance"
+		}
+	}
+
+	println("S2R close")
+	close(act)
+}
+
+// Room inits room.
+func Room(extent Rect, robot Step2Robot, act chan Action, rep chan Step2Robot) {
+	s2r := Step2Robot{}
+	Step1Robot.Dir = robot.Dir
+	Step1Robot.X, Step1Robot.Y = int(robot.Easting), int(robot.Northing)
+
+	for a := range act {
+		switch a {
+		case "Right":
+			Right()
+		case "Left":
+			Left()
+		case "Advance":
+			Advance()
+		}
+	}
+
+	s2r.Dir = Step1Robot.Dir
+	s2r.Pos = Pos{RU(Step1Robot.X), RU(Step1Robot.Y)}
+	println("Sent report")
+	rep <- s2r
 }
