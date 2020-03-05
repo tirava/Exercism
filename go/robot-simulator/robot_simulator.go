c996b33dba25f638074893160a250e8d7b9d0cb2
Eugene Klimov <tirava@gmail.com>
2020-03-05 17:17:27 +0300

    robot start

---
+++ go/robot-simulator/robot_simulator.go
@@ -0,0 +1,30 @@
+// Package robot implements robot simulator.
+package robot
+
+// String is stringer for direction.
+func (d Dir) String() string {
+	return "111"
+}
+
+// global directions.
+var (
+	N Dir
+	S Dir
+	W Dir
+	E Dir
+)
+
+// Left stepping.
+func Left() {
+
+}
+
+// Right stepping.
+func Right() {
+
+}
+
+// Advance for stepping.
+func Advance() {
+
+}
