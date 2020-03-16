ebe76292108114c9d8e667ecf1b92ef7578d0b33
Eugene Klimov <tirava@gmail.com>
2020-03-16 18:52:20 +0300

    change draft

--- go/change/change.go
+++ go/change/change.go
@@ -3,12 +3,30 @@ package change
 
 import "fmt"
 
+var result = make([][]int, 0)
+
 // Change returns coins.
 func Change(cost []int, coin int) ([]int, error) {
-	count := make([]int, len(cost))
+	var indexCount int
+	minCount := 99999
 	N := len(cost)
+	count := make([]int, N)
 
 	change(N-1, coin, count, cost)
+
+	for i, counts := range result {
+		var min int
+		for _, count := range counts {
+			min += count
+		}
+
+		if min < minCount {
+			minCount = min
+			indexCount = i
+		}
+	}
+
+	fmt.Println(result[indexCount])
 
 	return count, nil
 }
@@ -16,7 +34,10 @@ func Change(cost []int, coin int) ([]int, error) {
 func change(maxcoin, sum int, count, cost []int) {
 
 	if sum == 0 {
-		fmt.Println(maxcoin, sum, count, cost)
+		//fmt.Println(maxcoin, sum, count, cost)
+		copyCount := make([]int, len(count))
+		copy(copyCount, count)
+		result = append(result, copyCount)
 		return
 	}
 
