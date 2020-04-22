c9c42f9e08f8f380dc631186e5af405581270222
Eugene Klimov <tirava@gmail.com>
2020-04-22 17:50:33 +0300

    finding-1

--- go/zebra-puzzle/zebra_puzzle.go
+++ go/zebra-puzzle/zebra_puzzle.go
@@ -31,22 +31,22 @@ var (
 	animals   = [...]string{"snails", "fox", "dog", "horse", "zebra"}
 )
 
-var conditions = [...]house{ // 1. There are five houses.
-	{national: "Englishman", color: "red"}, // 2. The Englishman lives in the red house.
-	{national: "Spaniard", animal: "dog"},  // 3. The Spaniard owns the dog.
-	{color: "green", drink: "coffee"},      // 4. Coffee is drunk in the green house.
-	{national: "Ukrainian", drink: "tea"},  // 5. The Ukrainian drinks tea.
-	// {color: "ivory", position: x}, {color: "green", position: x+1}, // 6. The green house is immediately to the right of the ivory house.
-	{smoke: "Old Gold", animal: "snails"}, // 7. The Old Gold smoker owns snails.
-	{color: "yellow", smoke: "Kools"},     // 8. Kools are smoked in the yellow house.
-	{position: 3, drink: "milk"},          // 9. Milk is drunk in the middle house.
-	{national: "Norwegian", position: 1},  // 10. The Norwegian lives in the first house.
-	// {position: y, animal: "fox"}, {position: y+1, smoke: "Chesterfields"}, // 11. The man who smokes Chesterfields lives in the house next to the man with the fox.
-	// {position: z, animal: "horse"}, {position: z+1, smoke: "Kools"}, // 12. Kools are smoked in the house next to the house where the horse is kept.
-	{smoke: "Lucky Strike", drink: "orange juice"}, // 13. The Lucky Strike smoker drinks orange juice.
-	{national: "Japanese", smoke: "Parliaments"},   // 14. The Japanese smokes Parliaments.
-	// {position: n, color: "blue"}, {national: "Norwegian", position: n + 1}, // 15. The Norwegian lives next to the blue house.
-}
+//var conditions = [...]house{ // 1. There are five houses.
+//	{national: "Englishman", color: "red"}, // 2. The Englishman lives in the red house.
+//	{national: "Spaniard", animal: "dog"},  // 3. The Spaniard owns the dog.
+//	{color: "green", drink: "coffee"},      // 4. Coffee is drunk in the green house.
+//	{national: "Ukrainian", drink: "tea"},  // 5. The Ukrainian drinks tea.
+//	// {color: "ivory", position: x}, {color: "green", position: x+1}, // 6. The green house is immediately to the right of the ivory house.
+//	{smoke: "Old Gold", animal: "snails"}, // 7. The Old Gold smoker owns snails.
+//	{color: "yellow", smoke: "Kools"},     // 8. Kools are smoked in the yellow house.
+//	{position: 3, drink: "milk"},          // 9. Milk is drunk in the middle house.
+//	{national: "Norwegian", position: 1},  // 10. The Norwegian lives in the first house.
+//	// {position: y, animal: "fox"}, {position: y+1, smoke: "Chesterfields"}, // 11. The man who smokes Chesterfields lives in the house next to the man with the fox.
+//	// {position: z, animal: "horse"}, {position: z+1, smoke: "Kools"}, // 12. Kools are smoked in the house next to the house where the horse is kept.
+//	{smoke: "Lucky Strike", drink: "orange juice"}, // 13. The Lucky Strike smoker drinks orange juice.
+//	{national: "Japanese", smoke: "Parliaments"},   // 14. The Japanese smokes Parliaments.
+//	// {position: n, color: "blue"}, {national: "Norwegian", position: n + 1}, // 15. The Norwegian lives next to the blue house.
+//}
 
 func fact(n int) int {
 	switch n {
@@ -154,7 +154,7 @@ func generateHouses() {
 							}
 
 							count64++
-							spinner(count64)
+							fmt.Printf("%d of %d\r", count64, 120*120*120*120*120*120)
 
 							if checkHouses(hh) {
 								count++
@@ -174,18 +174,10 @@ func generateHouses() {
 	fmt.Println("Houses:", count)
 }
 
-func spinner(count uint64) {
-	//for {
-	//	for _, r := range "-\\|/" {
-	//		fmt.Printf("%c\r", r)
-	//		time.Sleep(delay)
-	//	}
-	//}
-	fmt.Printf("%d of %d\r", count, 120*120*120*120*120*120)
-}
-
 func checkHouses(hh [num]house) bool {
-	var c2, c3, c4, c5, cx, cy, cz int
+	var c2, c3, c4, c5 int //, cx, cy, cz int
+	var c7, c8, c9, c10 int
+	var c13, c14 int
 
 	for _, h := range hh {
 		if h.national == "Englishman" && h.color == "red" {
@@ -200,25 +192,46 @@ func checkHouses(hh [num]house) bool {
 		if h.national == "Ukrainian" && h.drink == "tea" {
 			c5++
 		}
+		// c6
+		if h.smoke == "Old Gold" && h.animal == "snails" {
+			c7++
+		}
+		if h.color == "yellow" && h.smoke == "Kools" {
+			c8++
+		}
+		if h.position == 3 && h.drink == "milk" {
+			c9++
+		}
+		if h.national == "Norwegian" && h.position == 1 {
+			c10++
+		}
+		// c11
+		// c12
+		if h.smoke == "Lucky Strike" && h.drink == "orange juice" {
+			c13++
+		}
+		if h.national == "Japanese" && h.smoke == "Parliaments" {
+			c14++
+		}
+		// c15
 
-		if h.national == "Norwegian" && h.drink == "water" {
-			cx++
-		}
-		if h.national == "Japanese" && h.animal == "zebra" { //&& h.position == 5 {
-			cy++
-		}
-
-		if h.position == 1 && h.national == "Norwegian" && h.drink == "water" &&
-			h.color == "yellow" && h.animal == "fox" && h.smoke == "Kools" {
-			cz++
-		}
+		//if h.national == "Norwegian" && h.drink == "water" && h.position == 1 {
+		//	cx++
+		//}
+		//if h.national == "Japanese" && h.animal == "zebra" && h.position == 5 {
+		//	cy++
+		//}
+		//
+		//if h.position == 1 && h.national == "Norwegian" && h.drink == "water" &&
+		//	h.color == "yellow" && h.animal == "fox" && h.smoke == "Kools" {
+		//	cz++
+		//}
 	}
 
-	//fmt.Println(c2, c3, c4, c5)
-	//return c2 == 1 && c3 == 1 && c4 == 1 && c5 == 1
-	//return c5 == 1 && c4 == 1 && c3 == 1
+	return c2 == 1 && c3 == 1 && c4 == 1 && c5 == 1 && c7 == 1 && c8 == 1 && c9 == 1 &&
+		c10 == 1 && c13 == 1 && c14 == 1
 	//return cx > 0 && cy > 0 //&& c2 > 0 && c3 > 0
-	return cz > 0 //&& cy > 0
+	//return cz > 0 //&& cy > 0
 }
 
 // SolvePuzzle solves puzzle.
