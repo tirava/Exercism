f9470df8465f36c09eeb876418060feac975befe
Eugene Klimov <tirava@gmail.com>
2020-04-25 17:34:05 +0300

    rect start

---
+++ go/rectangles/rectangles.go
@@ -0,0 +1,10 @@
+// Package rectangles implements counting the rectangles from ASCII diagram
+package rectangles
+
+func Count(input []string) int {
+	if len(input) < 2 {
+		return 0
+	}
+
+	return 0
+}
