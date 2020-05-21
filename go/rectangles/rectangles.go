a24920109531a72496f987f3b46bc49893a4167b
Eugene Klimov <tirava@gmail.com>
2020-05-21 17:04:24 +0300

    rect draft-1

--- go/rectangles/rectangles.go
+++ go/rectangles/rectangles.go
@@ -2,7 +2,6 @@
 package rectangles
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -13,39 +12,63 @@ func Count(input []string) int {
 		return count
 	}
 
-	lenLine := len(input[0])
+	//lenLine := len(input[0])
 	lenInput := len(input)
 
 	for i, line := range input {
-		for {
-			i1 := strings.Index(line, "+")
+		numPluses := strings.Count(line, "+")
+		pluses := make([]int, 0, numPluses)
+		for i := 0; i < numPluses; i++ {
+			pp := strings.Index(line, "+")
+			pluses = append(pluses, pp)
 			line = strings.Replace(line, "+", "*", 1)
+		}
 
-			if i1 == lenLine-1 || i1 == -1 {
-				break
-			}
+		for p1 := 0; p1 < numPluses-1; p1++ {
+			for p2 := p1 + 1; p2 < numPluses; p2++ {
+				for j := i + 1; j < lenInput; j++ {
+					j1, j2 := input[j][pluses[p1]], input[j][pluses[p2]]
 
-			i2 := strings.Index(line, "+")
-			line = strings.Replace(line, "+", "*", 1)
-			//fmt.Println(i1, i2, line)
+					if j1 == '+' && j2 == '+' {
+						count++
+						//break
+						continue
+					}
 
-			for j := i + 1; j < lenInput; j++ {
-				j1, j2 := input[j][i1], input[j][i2]
-				fmt.Println(string(j1), string(j2))
-
-				if j1 == '+' && j2 == '+' {
-					count++
-					break
+					if j1 != '|' || j2 != '|' {
+						break
+					}
 				}
-
-				if j1 != '|' || j2 != '|' {
-					continue
-				}
-
-				//fmt.Println(string(j1), string(j2))
 			}
 		}
 
+		//for {
+		//i1 := strings.Index(line, "+")
+		//line = strings.Replace(line, "+", "*", 1)
+		//
+		//if i1 == lenLine-1 || i1 == -1 {
+		//	break
+		//}
+		//
+		//i2 := strings.Index(line, "+")
+		//line = strings.Replace(line, "+", "*", 1)
+		//fmt.Println(i1, i2, line)
+
+		//for j := i + 1; j < lenInput; j++ {
+		//	j1, j2 := input[j][i1], input[j][i2]
+		//	//fmt.Println(string(j1), string(j2))
+		//
+		//	if j1 == '+' && j2 == '+' {
+		//		count++
+		//		break
+		//	}
+		//
+		//	if j1 != '|' || j2 != '|' {
+		//		continue
+		//	}
+		//
+		//	//fmt.Println(string(j1), string(j2))
+		//}
 	}
 
 	return count
