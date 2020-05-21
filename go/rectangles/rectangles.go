13f7a43d880db32f867ca7f2db782294ce3e8cfa
Eugene Klimov <tirava@gmail.com>
2020-05-21 14:56:06 +0300

    rect draft

--- go/rectangles/rectangles.go
+++ go/rectangles/rectangles.go
@@ -1,10 +1,52 @@
 // Package rectangles implements counting the rectangles from ASCII diagram
 package rectangles
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Count(input []string) int {
+	var count int
+
 	if len(input) < 2 {
-		return 0
+		return count
 	}
 
-	return 0
+	lenLine := len(input[0])
+	lenInput := len(input)
+
+	for i, line := range input {
+		for {
+			i1 := strings.Index(line, "+")
+			line = strings.Replace(line, "+", "*", 1)
+
+			if i1 == lenLine-1 || i1 == -1 {
+				break
+			}
+
+			i2 := strings.Index(line, "+")
+			line = strings.Replace(line, "+", "*", 1)
+			//fmt.Println(i1, i2, line)
+
+			for j := i + 1; j < lenInput; j++ {
+				j1, j2 := input[j][i1], input[j][i2]
+				fmt.Println(string(j1), string(j2))
+
+				if j1 == '+' && j2 == '+' {
+					count++
+					break
+				}
+
+				if j1 != '|' || j2 != '|' {
+					continue
+				}
+
+				//fmt.Println(string(j1), string(j2))
+			}
+		}
+
+	}
+
+	return count
 }
