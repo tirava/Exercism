8557cad8a38c5598ea62d71af3eb7e8f966b40b4
Eugene Klimov <tirava@gmail.com>
2020-05-21 17:54:01 +0300

    start two buckets

---
+++ go/two-bucket/two_bucket.go
@@ -0,0 +1,11 @@
+// Package twobucket demonstrates
+// how to measure an exact number of liters
+//by strategically transferring liters of fluid between the buckets.
+package twobucket
+
+// Solve solves the problem.
+func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (
+	goalBucket string, numSteps, otherBucketLevel int, e error) {
+
+	return "", 0, 0, nil
+}
