0ae9ddf5b980a04869220f014edb5fe4a9183b8b
Eugene Klimov <tirava@gmail.com>
2020-05-22 14:31:34 +0300

    two buckets ready

--- go/two-bucket/two_bucket.go
+++ go/two-bucket/two_bucket.go
@@ -5,7 +5,6 @@ package twobucket
 
 import (
 	"errors"
-	"fmt"
 )
 
 type bucket struct {
@@ -25,6 +24,10 @@ func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (
 		return "", 0, 0, errors.New("invalid data")
 	}
 
+	if sizeBucketTwo/sizeBucketOne > 1 && sizeBucketOne != 1 {
+		return "", 0, 0, errors.New("not solving")
+	}
+
 	steps = 0
 	b1 := bucket{size: sizeBucketOne, goal: goalAmount}
 	b2 := bucket{size: sizeBucketTwo, goal: goalAmount}
@@ -42,22 +45,11 @@ func smallFirst(b1, b2 bucket) (
 	for {
 		// pure to big while big not full
 		for b2.now != b2.size {
-			//// fill small
-			//if b1.fill() {
-			//	return "one", steps, b2.now, nil
-			//}
-			//fmt.Println("fill b1:", b1, b2)
-
 			// pure to big
 			g1, g2 := b1.pure(&b2)
-			//fmt.Println("pure b1 -> b2:", b1, b2)
 			if g1 {
-				//fmt.Println("final g1:", b1, b2)
-				//fmt.Println("-----------------")
 				return "one", steps, b2.now, nil
 			} else if g2 {
-				//fmt.Println("final g2:", b1, b2)
-				//fmt.Println("-----------------")
 				return "two", steps, b1.now, nil
 			}
 
@@ -65,27 +57,10 @@ func smallFirst(b1, b2 bucket) (
 			if b1.fill() {
 				return "one", steps, b2.now, nil
 			}
-			//fmt.Println("fill b1:", b1, b2)
 		}
 
 		b2.empty()
-		//fmt.Println("empty b2:", b1, b2)
-		//time.Sleep(time.Second)
-		//fmt.Println("steps:", steps)
 	}
-
-	// empty big
-	//b2.empty()
-
-	// pure to big
-	//b1.pure(&b2)
-	//if g1 {
-	//	return "one", steps, b2.now, nil
-	//} else if g2 {
-	//	return "two", steps, b1.now, nil
-	//}
-
-	return "", 0, 0, errors.New("todo smallFirst")
 }
 
 func bigFirst(b1, b2 bucket) (
@@ -96,31 +71,19 @@ func bigFirst(b1, b2 bucket) (
 		if b2.fill() {
 			return "two", steps, b1.now, nil
 		}
-		//fmt.Println("fill b2:", b1, b2)
 
 		// pure to small + empty small while big not empty
 		for b2.now != 0 {
 			g2, g1 := b2.pure(&b1)
 			if g1 {
-				//fmt.Println("final g1:", b1, b2)
 				return "one", steps, b2.now, nil
 			} else if g2 {
-				//fmt.Println("final g2:", b1, b2)
 				return "two", steps, b1.now, nil
 			}
-			//fmt.Println("pure b2 -> b1:", b1, b2)
 
 			b1.empty()
-			//fmt.Println("empty b1:", b1, b2)
-
-			//time.Sleep(time.Second)
-			//fmt.Println("steps:", steps)
 		}
 	}
-
-	fmt.Println("final:", b1, b2)
-
-	return "", 0, 0, errors.New("todo bigFirst")
 }
 
 func (b *bucket) fill() bool {
