bc986f353ab4fac889c1e98ec1052587eaf4c072
Eugene Klimov <tirava@gmail.com>
2020-06-28 15:02:39 +0300

    react ready

--- go/react/react.go
+++ go/react/react.go
@@ -1,147 +1,111 @@
 // Package react implements a basic reactive system.
 package react
 
-type react struct {
-	ic []inputCell
-	*computeCell
+type reactor struct {
+	registry []*computeCell
 }
 
 type inputCell struct {
-	cell
-	cc  *computeCell
-	ics *[]inputCell
+	value int
+	r     *reactor
 }
 
 type computeCell struct {
-	cell
-	cancel
+	compute   func() int
+	callbacks map[int]func(int)
+	prevVal   int
 }
 
-type cancel struct {
-	callback func(int)
+type canceler struct {
+	id           int
+	registeredTo *computeCell
 }
 
-type cell struct {
-	value    int
-	compute1 func(int) int
-	compute2 func(int, int) int
-}
-
-// New returns new react.
+// New returns new reactor.
 func New() Reactor {
-	c := cell{
-		compute1: func(i int) int {
-			return i
-		},
-		compute2: func(i, j int) int {
-			return 0
-		},
+	return &reactor{
+		registry: make([]*computeCell, 0),
 	}
-
-	cc := computeCell{
-		cell: c,
-	}
-
-	//ic := inputCell{
-	//	cell: c,
-	//	cc:   &cc,
-	//}
-
-	r := &react{
-		computeCell: &cc,
-		ic:          make([]inputCell, 0, 2),
-	}
-
-	//r.ic = append(r.ic, ic)
-
-	return r
 }
 
 // CreateInput creates an input cell.
-func (r *react) CreateInput(value int) InputCell {
-	//c := cell{
-	//	compute1: func(i int) int {
-	//		return i
-	//	},
-	//	compute2: func(i, j int) int {
-	//		return 0
-	//	},
-	//}
-
-	ic := inputCell{
-		cell: cell{value: value},
-		cc:   r.computeCell,
-		ics:  &r.ic,
+func (r *reactor) CreateInput(i int) InputCell {
+	return &inputCell{
+		value: i,
+		r:     r,
 	}
-
-	r.ic = append(r.ic, ic)
-	//r.ic[len(r.ic)-1].SetValue(value)
-	//r.ic[len(r.ic)-1].value = value
-	//fmt.Printf("cretae: %p %d\n", &r.ic[len(r.ic)-1], r.ic[len(r.ic)-1].value)
-
-	return &r.ic[len(r.ic)-1]
 }
 
-// CreateCompute1 creates a compute cell 1.
-func (r *react) CreateCompute1(cell Cell, calc func(int) int) ComputeCell {
-	r.computeCell.cell.compute1 = calc
-	//r.ic[0].SetValue(cell.Value())
-	r.ic[0].value = cell.Value()
-	r.computeCell.cell.value = r.computeCell.cell.compute1(cell.Value())
-
-	return r.computeCell
+// CreateCompute1 creates a compute 1 cell.
+func (r *reactor) CreateCompute1(c Cell, compute func(int) int) ComputeCell {
+	return r.createCompute(
+		func() int {
+			return compute(c.Value())
+		},
+	)
 }
 
-// CreateCompute1 creates a compute cell 2.
-func (r *react) CreateCompute2(c1 Cell, c2 Cell, calc func(int, int) int) ComputeCell {
-	r.computeCell.cell.compute2 = calc
-	//r.inputCell.SetValue(c1.Value(), c2.Value())
-	r.computeCell.cell.value = r.computeCell.cell.compute2(c1.Value(), c2.Value())
-
-	//fmt.Println(c1.Value(), c2.Value(), "->", r.computeCell.cell.value)
-
-	return r.computeCell
+// CreateCompute2 creates a compute 2 cell.
+func (r *reactor) CreateCompute2(c1, c2 Cell, compute func(int, int) int) ComputeCell {
+	return r.createCompute(
+		func() int {
+			return compute(c1.Value(), c2.Value())
+		},
+	)
 }
 
-// SetValue sets the value of the cell.
-func (ic *inputCell) SetValue(value int) {
-	ic.cell.value = value
-	if len(*ic.ics) == 1 {
-		ic.cc.cell.value = ic.cc.cell.compute1(value)
+func (r *reactor) createCompute(compute func() int) *computeCell {
+	var cc computeCell
 
-		return
-	}
+	cc.compute = compute
+	cc.callbacks = make(map[int]func(int))
+	cc.prevVal = cc.Value()
+	r.registry = append(r.registry, &cc)
 
-	for i := range *ic.ics {
-		//fmt.Printf("I: %d, addresses: %p %d %p %d\n", i, &(*ic.ics)[i], (*ic.ics)[i].value, ic, ic.cell.value)
-		if &(*ic.ics)[i] != ic {
-			ic.cc.cell.value = ic.cc.cell.compute2(value, (*ic.ics)[i].value)
-			//	//fmt.Println("addresses:", &c, ic)
-		}
-		//} else {
-		//	//fmt.Println("---------------", &c, ic)
-		//}
-	}
-
-	//ic.cc.cell.value = ic.cc.cell.compute2(value, ic.ics[1])
-	//ic.cc.cell.value = ic.cc.cell.compute2(value)
-	//fmt.Println(value, "->", ic.cc.cell.value, *ic.ics)
+	return &cc
 }
 
-// Value returns cell value.
-func (c *cell) Value() int {
-	return c.value
+// SetValue sets the value on an input cell.
+func (ic *inputCell) SetValue(i int) {
+	ic.value = i
+	ic.r.update()
 }
 
-// AddCallback adds a callback.
+// Value returns the value of an input cell.
+func (ic *inputCell) Value() int {
+	return ic.value
+}
+
+// Value returns the value of a compute cell.
+func (cc *computeCell) Value() int {
+	return cc.compute()
+}
+
+// AddCallback adds a callback function to a compute cell.
 func (cc *computeCell) AddCallback(cb func(int)) Canceler {
-	//cc.callback = calc
-	return &cc.cancel
+	cc.callbacks[len(cc.callbacks)] = cb
+
+	return &canceler{
+		id:           len(cc.callbacks) - 1,
+		registeredTo: cc,
+	}
 }
 
-// Cancel removes callbacks.
-func (c *cancel) Cancel() {
-	//c.callback = func(i int) int {
-	//	return i
-	//}
+// Cancel removes a callback function from a compute cell.
+func (c *canceler) Cancel() {
+	delete(c.registeredTo.callbacks, c.id)
+}
+
+func (r *reactor) update() {
+	for _, cc := range r.registry {
+		if len(cc.callbacks) == 0 || cc.Value() == cc.prevVal {
+			continue
+		}
+
+		for _, cb := range cc.callbacks {
+			cb(cc.Value())
+		}
+
+		cc.prevVal = cc.Value()
+	}
 }
